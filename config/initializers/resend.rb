51ca470f553e3e95bea8277a472bf6cf5863f1fe
kaoru <k24cal@yahoo.co.jp>
2026-04-11 22:25:00 +0900

    fix:mailer設定

--- config/initializers/resend.rb
+++ config/initializers/resend.rb
@@ -1,3 +1,9 @@
 require "resend"
 
-Resend.api_key = ENV.fetch("RESEND_API_KEY")
+resend_api_key = ENV["RESEND_API_KEY"]
+
+if resend_api_key.present?
+  Resend.api_key = resend_api_key
+elsif Rails.env.production?
+  raise KeyError, "key not found: \"RESEND_API_KEY\""
+end
