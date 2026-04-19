18ef708b02304f24a755b836577179482f149323
kaoru <k24cal@yahoo.co.jp>
2026-04-19 23:23:03 +0900

    initializers/openai.rb修正

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,6 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai[:api_key]
+  # config.access_token = Rails.application.credentials.openai[:api_key]
+  # credentialsがない場合は環境変数にフォールバック
+  config.access_token = Rails.application.credentials.dig(:openai, :api_key) ||
+                        ENV["OPENAI_API_KEY"]
 end
