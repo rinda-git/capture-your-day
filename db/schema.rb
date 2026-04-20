9746fed46070baa3e220ef429a3cc5ff99b1f283
kaoru <k24cal@yahoo.co.jp>
2026-04-20 17:51:21 +0900

    mistakeのmistake_stypeのデータ型を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_20_035408) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_20_081230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_20_035408) do
     t.text "explanation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "mistake_type"
+    t.integer "mistake_type"
     t.index ["journal_id"], name: "index_mistakes_on_journal_id"
     t.index ["user_id"], name: "index_mistakes_on_user_id"
   end
