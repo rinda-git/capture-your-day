e43c0ed1d91d1e5b28f3cccfaf09e6a5c6856b48
kaoru <k24cal@yahoo.co.jp>
2026-06-08 14:01:01 +0900

    RuboCopの指摘を修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only:[:privacy, :terms]
+  skip_before_action :authenticate_user!, only: [ :privacy, :terms ]
 
   def privacy
   end
