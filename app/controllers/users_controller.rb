5255d6e5d1ec0012f6ff21f985f2afc4957bea1b
kaoru <k24cal@yahoo.co.jp>
2026-06-10 00:04:26 +0900

    プロフィール編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to mypage_path, success: "プロフィールを更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
