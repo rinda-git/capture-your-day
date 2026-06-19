5df3990c76bbae35127091103d35ecef2de38892
kaoru <k24cal@yahoo.co.jp>
2026-06-19 12:02:41 +0900

    fix:アカウント削除後の遷移先の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def destroy
     current_user.destroy
     reset_session
-    redirect_to unauthenticated_path, notice: "退会が完了しました"
+    redirect_to unauthenticated_root_path, notice: "退会が完了しました"
   end
 
   private
