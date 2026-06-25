d66ca9925fec344d50ddc1033d259fa2cc49bff4
kaoru <k24cal@yahoo.co.jp>
2026-06-25 14:30:33 +0900

    lintエラーを修正

--- db/migrate/20260625050242_remove_unused_columns.rb
+++ db/migrate/20260625050242_remove_unused_columns.rb
@@ -3,6 +3,6 @@ class RemoveUnusedColumns < ActiveRecord::Migration[8.1]
     remove_column :journal_corrections, :advice, :text
     remove_column :journal_corrections, :mistake_patterns, :json
     remove_column :journal_corrections, :native_phrases, :json
-    remove_column :journal_corrections, :strengths, :json 
+    remove_column :journal_corrections, :strengths, :json
   end
 end
