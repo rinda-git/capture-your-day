4b4ab6f0a65da4e2cf39d7caf65238ef264d215f
kaoru <k24cal@yahoo.co.jp>
2026-07-01 15:03:01 +0900

    fix:音声読み上げボタン設置

--- app/javascript/controllers/speech_controller.js
+++ app/javascript/controllers/speech_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["text", "rate"]
+  static targets = ["text", "rate", "playButton", "stopButton"]
 
 speak() {
   if(!("speechSynthesis" in window)) {
@@ -17,13 +17,38 @@ speak() {
   utterance.rate = rate
   utterance.pitch = 1
 
+  utterance.onend = () => {
+    this.showPlayButton()
+  }
+
+  utterance.onerror = () => {
+    this.showPlayButton()
+  }
+
   speechSynthesis.cancel()
   speechSynthesis.speak(utterance)
-}
+  this.showStopButton()
+ }
 
 stop() {
   if ("speechSynthesis" in window) {
     speechSynthesis.cancel()
   }
+  this.showPlayButton()
+ }
+
+ disconnect() {
+  if("speechSynthesis" in window)
+    speechSynthesis.cancel()
+ }
+
+showStopButton() {
+  this.playButtonTarget.classList.add("hidden")
+  this.stopButtonTarget.classList.remove("hidden")
+ }
+
+showPlayButton() {
+  this.stopButtonTarget.classList.add("hidden")
+  this.playButtonTarget.classList.remove("hidden")
  }
 }
