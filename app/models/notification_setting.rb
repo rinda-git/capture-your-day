42b66f665e0d8877a9c72f5f1dd0b719af130d6e
kaoru <k24cal@yahoo.co.jp>
2026-07-13 17:36:25 +0900

    未使用のscene_typeを通知設定から削除

--- app/models/notification_setting.rb
+++ app/models/notification_setting.rb
@@ -1,7 +1,5 @@
 class NotificationSetting < ApplicationRecord
   belongs_to :user
 
-  enum :scene_type, { light: 0, dark: 1 }
-
   validates :notification_time, presence: true, if: :reminder_enabled?
 end
