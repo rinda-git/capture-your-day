5928b836c2868fd0746c255869d9e1b24ac33e90
kaoru <k24cal@yahoo.co.jp>
2026-07-16 16:15:01 +0900

    fix:お気に入り登録を現在のユーザーに制限

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -23,8 +23,8 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    # mistakes テーブル全体から、指定されたidのMistakeを1件探す
-    @mistake = Mistake.find(params[:mistake_id])
+    # 「ログイン中ユーザーが作成したmistakesの中から、指定されたidのMistakeを探す
+    @mistake = current_user.mistakes.find(params[:mistake_id])
     # ログイン中のユーザーが、そのmistakeをお気に入りする
     current_user.favorite(@mistake)
     # redirect_back fallback_location: mistakes_path
